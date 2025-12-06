786615dec666c06fa6a11b05c5948da435fc3158
GuilhermeSilvaN <guilhermesilvaalvescardoso@gmail.com>
2025-12-05 23:28:28 -0300

    Service Layer, component registration

--- src/main/java/com/coursejava/coursespringjpa/repositories/UserRepository.java
+++ src/main/java/com/coursejava/coursespringjpa/repositories/UserRepository.java
@@ -2,7 +2,8 @@ package com.coursejava.coursespringjpa.repositories;
 
 import com.coursejava.coursespringjpa.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+//@Repository (opcional) //registro de componente
 public interface UserRepository extends JpaRepository<User,Long> {
     
 }
--- src/main/java/com/coursejava/coursespringjpa/resource/UserResource.java
+++ src/main/java/com/coursejava/coursespringjpa/resource/UserResource.java
@@ -1,18 +1,34 @@
 package com.coursejava.coursespringjpa.resource;
 
 import com.coursejava.coursespringjpa.models.User;
+import com.coursejava.coursespringjpa.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(2L, "Ana Paula", "mariaxxz@gmail.com", "(00)1 1234-5678", "123");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+
+        return ResponseEntity.ok().body(user);
     }
 }
