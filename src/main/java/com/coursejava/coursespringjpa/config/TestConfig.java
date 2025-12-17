a64e1e7aa620ff2b47b06a1763f05e9caf591595
GuilhermeSilvaN <guilhermesilvaalvescardoso@gmail.com>
2025-12-17 13:05:47 -0300

    Enum OrderStatus, atributo status como integer na classe Order

--- src/main/java/com/coursejava/coursespringjpa/config/TestConfig.java
+++ src/main/java/com/coursejava/coursespringjpa/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.coursejava.coursespringjpa.config;
 
+import com.coursejava.coursespringjpa.enums.OrderStatus;
 import com.coursejava.coursespringjpa.models.Order;
 import com.coursejava.coursespringjpa.models.User;
 import com.coursejava.coursespringjpa.repositories.OrderRepository;
@@ -27,17 +28,11 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "(22) 1 2345-6789", "0000");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "(00) 1 1234-5678", "1234");
 
-        Order order1 = new Order();
-        order1.setClient(u1);
-        order1.setMoment(Instant.parse("2025-12-15T12:44:00Z"));
+        Order order1 = new Order(u1, Instant.parse("2025-12-15T12:44:00Z"), OrderStatus.PAID);
 
-        Order order2 = new Order();
-        order2.setClient(u2);
-        order2.setMoment(Instant.parse("2025-12-15T12:47:00Z"));
+        Order order2 = new Order(u2, Instant.parse("2025-12-15T12:47:00Z"), OrderStatus.WAITING_PAYMENT);
 
-        Order order3 = new Order();
-        order3.setClient(u1);
-        order3.setMoment(Instant.parse("2025-12-15T12:48:00Z"));
+        Order order3 = new Order(u1, Instant.parse("2025-12-15T12:48:00Z"), OrderStatus.DELIVERED);
 
         userRepository.save(u1);
         userRepository.save(u2);
--- src/main/java/com/coursejava/coursespringjpa/models/Order.java
+++ src/main/java/com/coursejava/coursespringjpa/models/Order.java
@@ -1,5 +1,6 @@
 package com.coursejava.coursespringjpa.models;
 
+import com.coursejava.coursespringjpa.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -25,7 +26,15 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss'Z'", timezone="GMT")
     private Instant moment;
 
+    private Integer status;
+
     public Order() {}
+
+    public Order(User client, Instant moment, OrderStatus status) {
+        this.client = client;
+        this.moment = moment;
+        setOrderStatus(status);
+    }
 
     public Long getId() {
         return id;
@@ -49,5 +58,15 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(this.status);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.status = orderStatus.getCode();
+        }
     }
 }
